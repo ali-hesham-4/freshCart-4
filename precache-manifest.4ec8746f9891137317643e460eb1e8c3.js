d9bbfb1b9aec2229498cff387e0ea3a66d6f6a70
Ali Hesham <alibasher2013@gmail.com>
2024-04-04 00:37:36 +0200

    Updates

---
+++ precache-manifest.4ec8746f9891137317643e460eb1e8c3.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24fd3ea76ff1e5b3093f93a832fd483f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "377feaf52282c6aece2e",
+    "url": "/static/css/2.1db279ec.chunk.css"
+  },
+  {
+    "revision": "13071d2b2be7d82b08ec",
+    "url": "/static/css/main.7fd13ff8.chunk.css"
+  },
+  {
+    "revision": "377feaf52282c6aece2e",
+    "url": "/static/js/2.8e00e408.chunk.js"
+  },
+  {
+    "revision": "7ec40f8c3f656c2d27df",
+    "url": "/static/js/3.989c05ce.chunk.js"
+  },
+  {
+    "revision": "13071d2b2be7d82b08ec",
+    "url": "/static/js/main.aa434e65.chunk.js"
+  },
+  {
+    "revision": "c5059c2901e5566d26bf",
+    "url": "/static/js/runtime~main.32b44409.js"
+  },
+  {
+    "revision": "db677e961692420fce98af43e153e94b",
+    "url": "/static/media/Amazon_Pay-Logo.wine.db677e96.png"
+  },
+  {
+    "revision": "87e55e21ba866eaf11321e39bcafff78",
+    "url": "/static/media/American Express.87e55e21.png"
+  },
+  {
+    "revision": "e4940e2a74966892c29e6b1d4be7c755",
+    "url": "/static/media/Mastercard-Logo.wine.e4940e2a.png"
+  },
+  {
+    "revision": "50b53bd27682743faef568e91dd6d190",
+    "url": "/static/media/PayPal-Logo.wine.50b53bd2.png"
+  },
+  {
+    "revision": "755bb035f36dde39627a49226c2ccb32",
+    "url": "/static/media/error.755bb035.png"
+  },
+  {
+    "revision": "1931427c23716bb4ff98d03627f7137c",
+    "url": "/static/media/fa-brands-400.1931427c.ttf"
+  },
+  {
+    "revision": "b6356c957274676e6571c1ff5e11c9a8",
+    "url": "/static/media/fa-brands-400.b6356c95.woff2"
+  },
+  {
+    "revision": "023a4a925fa3fce0f66b769ef6bbb264",
+    "url": "/static/media/fa-regular-400.023a4a92.woff2"
+  },
+  {
+    "revision": "a0cc1c8265e3163aa654a5284ea11ace",
+    "url": "/static/media/fa-regular-400.a0cc1c82.ttf"
+  },
+  {
+    "revision": "25914cfeafced317e1a0372187fbb2b9",
+    "url": "/static/media/fa-solid-900.25914cfe.ttf"
+  },
+  {
+    "revision": "6c4eee562650e53cee32496bdfbe534b",
+    "url": "/static/media/fa-solid-900.6c4eee56.woff2"
+  },
+  {
+    "revision": "71808e147dc6d82c198a4ba292c0cb69",
+    "url": "/static/media/fa-v4compatibility.71808e14.ttf"
+  },
+  {
+    "revision": "c9e50ccabe9e36f370272197595ea1e5",
+    "url": "/static/media/fa-v4compatibility.c9e50cca.woff2"
+  },
+  {
+    "revision": "01e44e6a6ea33dd28fc38ab9b9a3cd53",
+    "url": "/static/media/freshcart-logo.01e44e6a.svg"
+  },
+  {
+    "revision": "5acd5231f7f0b6ae7195a8d9126abc1c",
+    "url": "/static/media/grocery-banner-2.5acd5231.jpeg"
+  },
+  {
+    "revision": "36cd3336bdbb0110b290ffe0f911b5da",
+    "url": "/static/media/grocery-banner.36cd3336.png"
+  },
+  {
+    "revision": "436fa48e30e9ad5ecbec555bc04e64a4",
+    "url": "/static/media/pngwing.com (1).436fa48e.png"
+  },
+  {
+    "revision": "0a3cc9bff98ffb22a2a10024d18552e0",
+    "url": "/static/media/pngwing.com.0a3cc9bf.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ec23b97c3c3c0517326378f627bf4053",
+    "url": "/static/media/slider-image-1.ec23b97c.jpeg"
+  },
+  {
+    "revision": "07e637f30b61063c855d506580e1f00b",
+    "url": "/static/media/slider-image-2.07e637f3.jpeg"
+  },
+  {
+    "revision": "0642acda0b75f2fb74f9f44f264ae631",
+    "url": "/static/media/slider-image-3.0642acda.jpeg"
+  }
+]);
